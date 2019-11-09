3eda3f564d49ab5c1945fbdbd6c7e17223a6eb45
Ali Badawy <alibadawy9634@gmail.com>
2019-11-09 08:01:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ app = Flask(__name__)
 @app.route("/weathercurrent/<lan>/<lat>")
 def weathercurrent(lan, lat):
     link = """
-         https://api.openweathermap.org/data/2.5/forecast/hourly?lat=""" + str(lan) + """&lon=""" + str(lat) + """&appid=efbcb9da9bd397d2be9c9b3213e70016
-         """
+          https://api.openweathermap.org/data/2.5/weather?lat=""" + str(lan) + """&lon=""" + str(lat) + """&appid=7ed4776295f2355723df6c8532184620"""
     r = requests.get(link)
+
+    dataDict = r.json()
+
+    # return dataDict
 
     dataDict = r.json()
     try:
@@ -18,30 +21,16 @@ def weathercurrent(lan, lat):
         windSpeed = dataDict["wind"]["speed"]
         windDeg = dataDict["wind"]["deg"]
         #here return equations values
-        return dataDict
+        return  {
+                   "Temp":temp,
+                   "windSpeed": windSpeed,
+                   "windDeg": windDeg
+              }
+    # except:
     except:
         return ("Error")
 
-@app.route("/weather4days/<lan>/<lat>")
-def weather4days(lan, lat):
-    link = """
-         https://api.openweathermap.org/data/2.5/forecast/hourly?lat=""" + str(lan) + """&lon=""" + str(lat) + """&appid=efbcb9da9bd397d2be9c9b3213e70016
-         """
-    r = requests.get(link)
 
-    dataDict = r.json()
-    try:
-        temp = []
-        windSpeed = []
-        windDeg = []
-        for i in range(3):
-            temp.append(dataDict["list"][i]["main"]["temp"])
-            windSpeed.append(dataDict["list"][i]["wind"]["speed"])
-            windDeg.append(dataDict["list"][i]["wind"]["deg"])
-        #return formula resutls
-        return dataDict
-    except:
-        return ("Error")
 
 if __name__ == "__main__":
     app.run(debug=True)
